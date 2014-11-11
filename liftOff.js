360fa5647783732ee434042c2513edbf28b1cb0a
Stephanie <sscelani@gmail.com>
2014-11-11 09:30:01 -0500

    Made an amendment to media query.

--- liftOff.js
+++ liftOff.js
@@ -1,12 +1,12 @@
 $.fn.liftOff = function(){
     var div=$(".preloader");
     $.when(
-    	$(".logo-container").animate({bottom: '40%'}).promise()
+    	$(".logo-container").animate({bottom: '35%'}).promise()
     ).done(function(){
-    	$(".loader-container").animate({bottom: '20%'}).promise
+    	$(".loader-container").animate({bottom: '15%'}).promise
     })
 
-    div.delay(4000).animate({bottom:'100%'}, 400);
+    div.delay(40000).animate({bottom:'100%'}, 400);
 
  };
 
